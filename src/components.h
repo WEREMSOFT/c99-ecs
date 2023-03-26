f3f749287a7e688ea8e20210a919d3ac3febbc64
WEREMSOFT <pablo.weremczuk@gmail.com>
2023-03-27 00:44:48 +0200

    added asset store and sprite component

--- src/components.h
+++ src/components.h
@@ -1,3 +1,5 @@
+#include "assetStore.h"
+
 typedef struct
 {
 	float x,y,z,w;
@@ -12,3 +14,33 @@ typedef struct
 {
 	Vector Velocity;
 } RigidBodyComponent;
+
+typedef struct
+{
+int width;
+	int height;
+	int zIndex;
+	bool isFixed;
+
+	TextureIdEnum assetId;
+	SDL_Rect srcRect;
+	SDL_RendererFlip flip;
+} SpriteComponent;
+
+SpriteComponent spriteComponentCreate(
+	const TextureIdEnum assetId, 
+	int with, int height, 
+	int zIndex, int srcRectX, int srcRectY, 
+	bool isFixed)
+{
+	SpriteComponent _this = {0};
+	_this.zIndex = zIndex;
+	_this.width = with;
+	_this.height = height;
+	_this.assetId = assetId;
+	_this.srcRect = (SDL_Rect){srcRectX, srcRectY, with, height};
+	_this.isFixed = isFixed;
+	_this.flip = SDL_FLIP_NONE;
+
+	return _this;
+}
