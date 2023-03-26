a15e84cfb0d530c7bed06812fe86aa5fba4e874f
WEREMSOFT <pablo.weremczuk@gmail.com>
2023-03-26 19:40:01 +0200

    implementation of game

--- src/game.h
+++ src/game.h
@@ -1,7 +1,9 @@
 #include <SDL2/SDL.h>
-
+#include <stdbool.h>
 
 #define MAX_ENTITIES 10
+const int FPS = 60;
+const int MILLISECONDS_PER_FRAME = 1000 / FPS;
 
 enum ComponentEnum
 {
@@ -47,54 +49,149 @@ Registry registryCreate()
 typedef struct
 {
 	Registry registry;
+	SDL_Window *window;
+	SDL_Renderer *renderer;
 	Vector windowSize;
+	int windowWidth;
+	int windowHeight;
+	int millisecondsPreviousFrame;
+	bool isRunning;
 } Game;
 
 Game gameCreate()
 {
-	Game returnValue = {0};
-	return returnValue;
-}
+	Game _this = {0};
+	_this.windowWidth = 800;
+	_this.windowHeight = 600;
+	_this.registry = registryCreate();
+	_this.isRunning = true;
 
-void gameInit(Game _this)
-{
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
-	{
-		Logger::Err("Error initializing SDL");
-		return;
-	}
-
-	if(TTF_Init() != 0)
-	{
-		Logger::Err("Error initializing SDL TTF.");
-		return;
-	}
+	assert(SDL_Init(SDL_INIT_EVERYTHING) == 0 && "Error initializing SDL\n");
 
 	SDL_DisplayMode displayMode;
 	SDL_GetCurrentDisplayMode(0, &displayMode);
 
-	_this.windowSize.x = 800;	// displayMode.w;
-	_this.windowSize.y = 600; // displayMode.h;
+	_this.windowSize.x = 800;
+	_this.windowSize.y = 600;
 
-	window = SDL_CreateWindow(
+	_this.window = SDL_CreateWindow(
 		NULL,
 		SDL_WINDOWPOS_CENTERED,
 		SDL_WINDOWPOS_CENTERED,
-		windowWidth,
-		windowHeight,
+		_this.windowWidth,
+		_this.windowHeight,
 		SDL_WINDOW_BORDERLESS);
 
-	if (!window)
+	assert(_this.window != NULL && "Error creating SDL window");
+
+ 	_this.renderer = SDL_CreateRenderer(_this.window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+	assert(_this.renderer != NULL && "Error creating renderer");
+
+	return _this;
+}
+
+Game gameInit(Game _this)
+{
+// Entity 1
 	{
-		Logger::Err("Error creating window");
-		return;
+		Entity entity = entityCreate(_this.registry);
+		entityAddComponent(entity, _this.registry, &((PositionComponent){3, 4}), COMPONENT_POSITION);
+		entityAddComponent(entity, _this.registry, &((RigidBodyComponent){10., 20.}), COMPONENT_RIGID_BODY);
+	}
+	// Entity 2
+	{
+		Entity entity = entityCreate(_this.registry);
+		entityAddComponent(entity, _this.registry, &((PositionComponent){1, 2}), COMPONENT_POSITION);
+		entityAddComponent(entity, _this.registry, &((RigidBodyComponent){30., 40.}), COMPONENT_RIGID_BODY);
+	}
+	return _this;
+}
+
+void gameDestroy(Game _this)
+{
+	SDL_DestroyRenderer(_this.renderer);
+	SDL_DestroyWindow(_this.window);
+	SDL_Quit();
+}
+
+Game gameProcessInput(Game _this)
+{
+	SDL_Event event;
+
+	while (SDL_PollEvent(&event))
+	{
+		// Handle SDL events
+		switch (event.type)
+		{
+		case SDL_QUIT:
+			_this.isRunning = false;
+			break;
+		case SDL_KEYDOWN:
+			if (event.key.keysym.sym == SDLK_ESCAPE)
+			{
+				_this.isRunning = false;
+			}
+			break;
+		}
+	}
+	return _this;
+}
+
+void gameRender(Game _this)
+{
+	SDL_SetRenderDrawColor(_this.renderer, 21, 21, 21, 255);
+	SDL_RenderClear(_this.renderer);
+	
+	SDL_RenderPresent(_this.renderer);
+}
+
+Game gameUpdate(Game _this)
+{
+	int currentTicks = SDL_GetTicks();
+	int timeToWait = MILLISECONDS_PER_FRAME - currentTicks + _this.millisecondsPreviousFrame;
+	if (timeToWait > 0 && timeToWait < MILLISECONDS_PER_FRAME)
+		SDL_Delay(timeToWait);
+
+	float deltaTime = (SDL_GetTicks() - _this.millisecondsPreviousFrame) / 1000.f;
+	_this.millisecondsPreviousFrame = SDL_GetTicks();
+
+	printf("########################################\n");
+	for(int i = 0; i < _this.registry.entities->size; i++)
+	{
+		Entity entity = *(Entity *)arrayGetElementAt(_this.registry.entities, i);
+		{
+			PositionComponent* position = entityGetComponent(entity, _this.registry, COMPONENT_POSITION);
+			printf("position: %d, %d\n", position->x, position->y);
+			RigidBodyComponent* rigidBody = entityGetComponent(entity, _this.registry, COMPONENT_RIGID_BODY);
+			printf("velocity: %f, %f\n", rigidBody->Velocity.x, rigidBody->Velocity.y);
+		}
 	}
 
-	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	if (!renderer)
-	{
-		Logger::Err("Error creating renderer");
-		return;
-	}
+	movementSystem(_this.registry);
 
+	printf("########################################\n");
+	for(int i = 0; i < _this.registry.entities->size; i++)
+	{
+		Entity entity = *(Entity *)arrayGetElementAt(_this.registry.entities, i);
+		{
+			PositionComponent* position = entityGetComponent(entity, _this.registry, COMPONENT_POSITION);
+			printf("position: %d, %d\n", position->x, position->y);
+			RigidBodyComponent* rigidBody = entityGetComponent(entity, _this.registry, COMPONENT_RIGID_BODY);
+			printf("velocity: %f, %f\n", rigidBody->Velocity.x, rigidBody->Velocity.y);
+		}
+	}
+	registryUpdate(_this.registry);
+	return _this;
+}
+
+Game gameRun(Game _this)
+{
+	while (_this.isRunning)
+	{
+		_this = gameProcessInput(_this);
+		_this = gameUpdate(_this);
+		gameRender(_this);
+	}
+	return _this;
 }
--- src/main.c
+++ src/main.c
@@ -6,49 +6,15 @@
 #include "core/bitset.h"
 
 #include <SDL2/SDL.h>
-#include <game.h>
+#include "game.h"
 
 int main()
 {
-	Registry registry = registryCreate();
-	// Entity 1
-	{
-		Entity entity = entityCreate(registry);
-		entityAddComponent(entity, registry, &((PositionComponent){3, 4}), COMPONENT_POSITION);
-		entityAddComponent(entity, registry, &((RigidBodyComponent){10., 20.}), COMPONENT_RIGID_BODY);
-	}
-	// Entity 2
-	{
-		Entity entity = entityCreate(registry);
-		entityAddComponent(entity, registry, &((PositionComponent){1, 2}), COMPONENT_POSITION);
-		entityAddComponent(entity, registry, &((RigidBodyComponent){30., 40.}), COMPONENT_RIGID_BODY);
-	}
+	Game game = gameCreate();
+	game = gameInit(game);
 
-	registryUpdate(registry);
+	game = gameRun(game);
 
-	printf("########################################\n");
-	for(int i = 0; i < registry.entities->size; i++)
-	{
-		Entity entity = *(Entity *)arrayGetElementAt(registry.entities, i);
-		{
-			PositionComponent* position = entityGetComponent(entity, registry, COMPONENT_POSITION);
-			printf("position: %d, %d\n", position->x, position->y);
-			RigidBodyComponent* rigidBody = entityGetComponent(entity, registry, COMPONENT_RIGID_BODY);
-			printf("velocity: %f, %f\n", rigidBody->Velocity.x, rigidBody->Velocity.y);
-		}
-	}
-
-	movementSystem(registry);
-
-	printf("########################################\n");
-	for(int i = 0; i < registry.entities->size; i++)
-	{
-		Entity entity = *(Entity *)arrayGetElementAt(registry.entities, i);
-		{
-			PositionComponent* position = entityGetComponent(entity, registry, COMPONENT_POSITION);
-			printf("position: %d, %d\n", position->x, position->y);
-			RigidBodyComponent* rigidBody = entityGetComponent(entity, registry, COMPONENT_RIGID_BODY);
-			printf("velocity: %f, %f\n", rigidBody->Velocity.x, rigidBody->Velocity.y);
-		}
-	}
+	gameDestroy(game);
+	
 }
